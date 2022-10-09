8e7567e3e0ef7aa6a94570fb0447e1daf7a644f9
enan7 <enan7@hotmail.com>
2022-10-09 22:39:19 +0500

    Application.yml replaced with application.properties . Route Configuration added

--- src/main/java/com/doctorcattle/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/doctorcattle/apigateway/ApiGatewayApplication.java
@@ -1,23 +1,12 @@
 package com.doctorcattle.apigateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaClient
-@RestController
 public class ApiGatewayApplication {
-	
-	 @RequestMapping("/")
-	  public String register() {
-	    return "registered";
-	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
---
+++ src/main/java/com/doctorcattle/apigateway/SpringCloudConfig.java
@@ -0,0 +1,103 @@
+package com.doctorcattle.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+	  @Bean
+	    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+	        return builder.routes()
+	                .route(r -> r.path("/api/customer-service/**")
+	                        .uri("lb://CUSTOMER-SERVICE")
+	                        )
+
+	                .route(r ->r.path("/api/cattle-service/**")
+	                        .uri("lb://CATTLE-SERVICE")
+	                        )
+	                .build();
+	    }
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	 }
